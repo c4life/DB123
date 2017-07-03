a001e49f14ef309d5506c82f7c136ac7fcfc8752
unknown <>
2017-07-02 23:20:48 -0700

    should be good

--- public_html/updateDatabase.php
+++ public_html/updateDatabase.php
@@ -1,10 +1,9 @@
 <?php
-
 	$formnumber = $_POST['formnumber'];
 	$week = $_POST['week'];
 	$day = $_POST['day'];
 	$crtdate = $_POST['crtdate'];
-
+	$editor = $_POST['editor'];
 
   $servername="localhost";
   $username="id2006420_admin";
@@ -19,11 +18,12 @@
   }
   echo "Connected successfully";
 
-	// insert the new bookmark into the db table
+	// insert the new form into the db table
   ;
-  $sql = "INSERT INTO `Form_info` (`FormNo`, `Week`, `Day`, `Crtdate`) VALUES ('$formnumber','$week','$day','$crtdate')";
+  $sql = "INSERT INTO `form_info` (`formno`, `week`, `day`, `crtdate`,`editor`) VALUES ('$formnumber','$week','$day','$crtdate','$editor')";
 
   if ($conn->query($sql) === TRUE) {
+			mysqli_set_charset($conn,"utf8");
       echo "New record created successfully";
   } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
@@ -37,22 +37,28 @@
 		</style>
 		<script type = "text/javascript">
 		</script>
+
+
 	</head>
 	<body>
-    <div id = "addLink">
+  <div id = "addLink">
 			<form name = "addLink" action = "updateSummary.php" method = "post">
 
+<p class = "addP">表格编号</p>
 			<input type = "text" name = "formnumber" value = "" autofocus>
-			<p class = "addP">Form Number</p>
 
+<p class = "addP">周数</p>
 			<input type = "text" name = "week">
-			<p class = "addP">Week</p>
 
+<p class = "addP">天数</p>
 			<input type = "text" name = "day">
-			<p class = "addP">Day</p>
 
-			<input type = "text" name = "crtdate">
-			<p class = "addP">Crtdate</p>
+<p class = "addP">创建日期</p>
+			<input type = "date" name = "crtdate">
+<p class = "addP">创建人</p>
+			<input type = "text" name = "editor">
+
+
 
 <p id = "error"></p>
 				<input id = "submit" class = "one" type = "submit" value = "Add Products" />
--- public_html/updateForm.php
+++ public_html/updateForm.php
@@ -10,17 +10,19 @@
     <div id = "addLink">
 			<form name = "addLink" action = "updateDatabase.php" method = "post">
 
-				<input type = "text" name = "formnumber" value = "" autofocus>
-				<p class = "addP">Form Number</p>
+				<p class = "addP">表格编号</p>
+							<input type = "text" name = "formnumber" value = "" autofocus>
 
-				<input type = "text" name = "week">
-				<p class = "addP">Week</p>
+				<p class = "addP">周数</p>
+							<input type = "text" name = "week">
 
-				<input type = "text" name = "day">
-				<p class = "addP">Day</p>
+				<p class = "addP">天数</p>
+							<input type = "text" name = "day">
 
-				<input type = "text" name = "crtdate">
-				<p class = "addP">Crtdate</p>
+				<p class = "addP">创建日期</p>
+							<input type = "date" name = "crtdate">
+				<p class = "addP">创建人</p>
+							<input type = "text" name = "editor">
 
 				<p id = "error"></p>
 				<input id = "submit" class = "one" type = "submit" value = "Add Products" />
